bf3645427688cbdc317a690baa10c2fad9a5123b
callmeJoe-prix <61330863+callmeJoe-prix@users.noreply.github.com>
2025-11-19 23:11:36 +0700

    Update simulasi.py

--- routes/simulasi.py
+++ routes/simulasi.py
@@ -1,24 +1,3 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Virtual Lab Fotosintesis",
-    layout="wide",
-    page_icon="🌱"
-)
-
-# Navigasi halaman
-pg = st.navigation(
-    [
-        st.Page("routes/pendahuluan.py", title="Pendahuluan", icon="🏠"),
-        st.Page("routes/teori.py", title="Teori Fotosintesis", icon="📘"),
-        st.Page("routes/simulasi.py", title="Simulasi Interaktif", icon="🧪"),
-        st.Page("routes/kuis.py", title="Kuis", icon="❓"),
-    ]
-)
-
-# Sidebar
-with st.sidebar:
-    st.write("## Menu :gear:")
-    st.write("Gunakan menu di atas untuk berpindah halaman.")
-
-pg.run()
+st.title("simulasi")
