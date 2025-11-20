b5049418d593e9f804a0a791865a5b2c6910792d
callmeJoe-prix <61330863+callmeJoe-prix@users.noreply.github.com>
2025-11-20 12:40:24 +0700

    Update simulasi.py

--- routes/simulasi.py
+++ routes/simulasi.py
@@ -1,21 +1,5 @@
 import streamlit as st
 
-# DEBUG
-st.write("DEBUG: simulasi.py loaded")
-try:
-    from models.simulation_engine import run_simulation
-    st.write("DEBUG: import simulation_engine OK")
-except Exception as e:
-    st.error(f"DEBUG: import error → {e}")
-
-st.write("DEBUG: simulasi.py loaded")
-
-try:
-    from simulation_engine import run_simulation
-    st.write("DEBUG: import simulation_engine OK")
-except Exception as e:
-    st.error(f"DEBUG: gagal import simulation_engine → {e}")
-
 
 # -----------------------------------------
 # TITLE
