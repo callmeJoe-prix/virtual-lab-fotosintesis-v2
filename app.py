e4458ee0b27f2731b1fa27b703da0c85cf45f859
callmeJoe-prix <61330863+callmeJoe-prix@users.noreply.github.com>
2025-11-22 11:12:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ pg = st.navigation(
         st.Page("routes/pendahuluan.py", title="Pendahuluan", icon="🏠"),
         st.Page("routes/teori.py", title="Teori Fotosintesis", icon="📘"),
         st.Page("routes/simulasi.py", title="Simulasi Interaktif", icon="🧪"),
-        st.Page("routes/kuis.py", title="Kuis", icon="❓"),
+        st.Page("routes/kuis.py", title="Kuis", icon="❓")
+        st.page(""routes/survey.py, title="Survey", icon= "❓"),
     ]
 )
 
